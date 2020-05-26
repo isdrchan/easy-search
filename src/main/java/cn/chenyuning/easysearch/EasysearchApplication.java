a4adf4733c9d4118c00a2045246b03dcc02ab39d
isdrchan <cynmsg@gmail.com>
2020-05-26 18:01:09 +0800

    learn

---
+++ src/main/java/cn/chenyuning/easysearch/EasysearchApplication.java
@@ -0,0 +1,13 @@
+package cn.chenyuning.easysearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasysearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasysearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/chenyuning/easysearch/EasysearchApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.chenyuning.easysearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasysearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
